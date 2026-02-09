3774e29070db0b842800d6192a1d84133e7d9f45
Seocgom301096 <xoxierdy@gmail.com>
2026-02-09 11:51:37 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,21 @@
+let num = Math.floor(Math.random() * 15);
+
+//3の倍数のとき
+if (num % 3 === 0) {
+    console.log("3の倍数です");
+}
+
+//5の倍数のとき
+else if (num % 5 === 0) {
+    console.log("5の倍数です");
+}
+
+//3と5の倍位数のとき
+if (num % 3 && num % 5 === 0) {
+    console.log("3と5の倍数です");
+}
+
+//それ以外(変数の値を出力)
+else { 
+    console.log(num);
+}
